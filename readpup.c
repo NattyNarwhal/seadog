353985a2c971c3c327b576437dc91b737c002c08
Calvin Buckley <calvin@cmpct.info>
2025-10-19 19:34:18 -0300

    it was cute pretending we were unix hackers
    make this a little more readable

--- readpup.c
+++ readpup.c
@@ -2,47 +2,45 @@
 #include <stdint.h>
 
 typedef struct Dawg {
-	FILE *f;
-	int s;
+	FILE *file;
+	int word_size;
 } Dawg;
 
 #define DAWG_FINAL (1 << 5)
 #define DAWG_EOL (1 << 6)
 
-int lookup(Dawg *d, int i, const char *s)
+int lookup(Dawg *dawg, int index, const char *string)
 {
-	int w; /* [iiii_iiii...]_iefc_cccc (2-4 bytes) */
-	char c;
-	const char *n;
-	n = s + 1;
-	if (0 != fseek(d->f, i * d->s, SEEK_SET)) {
+	int word; /* [iiii_iiii...]_iefc_cccc (2-4 bytes) */
+	char character;
+	if (0 != fseek(dawg->file, index * dawg->word_size, SEEK_SET)) {
 		return -1;
 	}
 	do {
-		w = 0; /* clear high bytes for compact */
-		if (1 == fread(&w, 1, d->s, d->f)) {
+		word = 0; /* clear high bytes for compact */
+		if (1 == fread(&word, 1, dawg->word_size, dawg->file)) {
 			return -1;
 		}
 		/* (swap on big) */
-		i = w >> 7;
-		c = (w & 0x1F) + 0x60; /* 26 -> 0x7A ('z') */
-		if (*s == c && *n == '\0' && (w & DAWG_FINAL)) {
+		index = word >> 7;
+		character = (word & 0x1F) + 0x60; /* 26 -> 0x7A ('z') */
+		if (string[0] == character && string[1] == '\0' && (word & DAWG_FINAL)) {
 			return 1;
-		} else if (*s == c && i) {
-			return lookup(d, i, n);
+		} else if (string[0] == character && index) {
+			return lookup(dawg, index, string + 1);
 		}
-	} while ((w & DAWG_EOL) == 0);
+	} while ((word & DAWG_EOL) == 0);
 	return 0;
 }
 
 int main(int argc, char **argv)
 {
-	Dawg d;
+	Dawg dawg;
 	int i;
-	d.f = fopen(argv[1], "rb");
-	d.s = fgetc(d.f) & 0x7; /* 2-4 */
+	dawg.file = fopen(argv[1], "rb");
+	dawg.word_size = fgetc(dawg.file) & 0x7; /* 2-4 */
 	for (i = 2; i < argc; i++) {
-		printf("%s? %d\n", argv[i], lookup(&d, 1, argv[i]));
+		printf("%s? %d\n", argv[i], lookup(&dawg, 1, argv[i]));
 	}
 	return 0;
 }
