052f10e7aa3678626042b2db8cd7bb14206dd101
Calvin Buckley <calvin@cmpct.info>
2025-10-20 19:01:59 -0300

    validation

--- dawg.c
+++ dawg.c
@@ -63,9 +63,10 @@ int init_dawg_file(Dawg *dawg, const char *filename)
 	if (!dawg->file) {
 		goto fail;
 	}
-	/* first byte (because LE) is DAWG_EOL | DAWG_EOL | byte_size */
+	/* first byte (because LE) is DAWG_FINAL | DAWG_EOL | byte_size */
 	dawg->word_size = fgetc(dawg->file) & 0x7; /* 2-4 */
-	if (dawg->word_size == EOF) {
+	if (dawg->word_size == EOF || dawg->word_size > 4
+			|| dawg->word_size < 2) {
 		goto fail;
 	}
 	return 0;
--- readpup.c
+++ readpup.c
@@ -6,6 +6,9 @@ int main(int argc, char **argv)
 {
 	Dawg dawg;
 	int i;
+	if (argc < 2) {
+		return 1;
+	}
 	init_dawg_file(&dawg, argv[1]);
 	for (i = 2; i < argc; i++) {
 		printf("%s? %d\n", argv[i], dawg_lookup(&dawg, argv[i]));
